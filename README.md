450549be33c232b62ca26067332a6fd3235d9381
matheuspdf <matheuslopes.pdf@gmail.com>
2023-08-02 19:51:37 -0300

    Dados dinâmicos, Templates/Css

--- README.md
+++ README.md
@@ -5,11 +5,13 @@
 3. Seu primeiro projeto Django ✔
 4. Modelos do Django ✔
 5. Django Admin ✔
-6. Deploy! ...
+6. Deploy! x
 7. URLs ✔
 8. Django Views - hora de criar! ✔
 9. Introdução ao HTML ✔
 10. QuerySets e ORM do Django ✔
-11. Dados dinâmicos em templates
-12. 
+11. Dados dinâmicos em templates ✔
+12. Templates do Django ✔
+13. CSS - deixe tudo mais bonito! ✔ 
+
 
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from .models import Post
 
 
-# Create your views here.
 def post_list(request):
-    posts = Post.objects.filter5(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
